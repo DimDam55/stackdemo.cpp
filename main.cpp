96b42934cb230de93654e9cf2da4f9c5583f6359
DimDam55 <munro546@gmail.com>
2020-03-20 18:43:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class stack{
+  public:
+    stack(int size){
+      T stack[size];
+    }
+    stack (const stack &s){
+      
+    }
+    ~stack(){}
+    const stack & operator = (const stack &s){}
+
+    bool empty(){}
+    void push(const T &x){}
+    T pop(){}
+    int size(){}
+
+    friend ostream & operator<<(ostream &out,const stack &s){}
+  private:
+    struct node{
+      int info;
+      node *next;
+    };
+    node *head;
+    
+}
