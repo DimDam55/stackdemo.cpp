5b0a2063f505e54d2a4009dc9fb3adbaefad221e
DimDam55 <munro546@gmail.com>
2020-03-20 21:01:47 +0000

    nothing

--- main.cpp
+++ main.cpp
@@ -6,25 +6,47 @@ template <typename T>
 class stack{
   public:
     stack(int size){
-      T stack[size];
+      head = NULL;
     }
     stack (const stack &s){
-      
+      head = s.head;
+      node p = head;
+            for(int i = 0; i<len-1; i++){
+
+      }
     }
     ~stack(){}
     const stack & operator = (const stack &s){}
 
-    bool empty(){}
-    void push(const T &x){}
-    T pop(){}
-    int size(){}
+    bool empty(){
+      return head == NULL;
+    }
+    void push(const T &x){
+      node *p = new node;
+      p->info = x;
+      p->next = head;
+      head = p;
+      len++;
+    }
+    T pop(){
+      node *q = head;
+      head = q->next;
+      delete q;
+      len--;
+    }
+    int size(){
+      return len;
+    }
 
-    friend ostream & operator<<(ostream &out,const stack &s){}
+    friend ostream & operator<<(ostream &out,const stack &s){
+      out<<
+    }
   private:
     struct node{
       int info;
       node *next;
     };
     node *head;
+    int len = 0;
     
 };
