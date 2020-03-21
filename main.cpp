9a8bf86d4b6c40cef0dd80db415317d08db7c7b8
DimDam55 <munro546@gmail.com>
2020-03-21 17:17:07 +0000

    the first functional version

--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@ class stack{
       data = new T[size];
       top = 0;
     }
-    stack (const stack &s){}
+    stack (const stack &s){
+      data = new T[s.top];
+      top = s.top;
+      for(int i = 0;i<top; i++){
+        data[i] = s.data[i];
+      }
+
+    }
 
     ~stack(){}
 
     const stack & operator = (const stack &s){
-      
+      top = s.top;
+      for(int i = 0; i<s.top; i++) data[i] = s.data[i];
+      return *this;
     }
 
     bool empty(){
@@ -24,16 +33,52 @@ class stack{
       data[top++] = x;
     }
     T pop(){
-      data[--top];
+      return data[--top];
     }
     int size(){
       return top;
     }
 
     friend ostream & operator<<(ostream &out,const stack &s){
-      out<<
+      if(s.top == 0) out<<"[]";
+      else{
+        out<<"[";
+        for(int i = 0; i<s.top-1; i++) out<<s.data[i]<<",";
+        out<<s.data[s.top - 1]<<"]";
+      }
+      return out;
     }
   private:
-    T data[];
+    T *data;
     int top; 
 };
+
+/*int main(){
+  stack<int>s(10);
+  cout<<"s is empty"<<s<<endl;
+  s.push(42);
+  cout<<"s has one element:"<<s<<endl;
+  s.push(17);
+  s.push(34);
+  cout<<"s has more elements:"<<s<<endl;
+  cout<<"how many? "<<s.size()<<endl;
+  stack<int>t(5);
+  t.push(7);
+  cout<<"t: "<<t<<endl;
+  t = s;
+  cout<<"popping from s: "<<s.pop()<<endl;
+  s.push(8);
+  stack<int>a(s);
+  t.push(99);
+  a.push(77);
+  cout<<"s: "<<s<<endl;
+  cout<<"t: "<<t<<endl;
+  cout<<"a: "<<a<<endl;
+  stack<double>c(4);
+  c.push(3.14);
+  c.push(1.414);
+  cout<<"c contains doubles: "<<c<<endl;
+  stack<char>k(4);
+  k.push('&');
+  cout<<"k contains a character: "<<k<<endl;
+}*/
